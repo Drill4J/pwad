bee112298d4f1daff1e84c0279d58fb538cb9d6e
RomanDavlyatshin <davlyatshinra@gmail.com>
2021-11-24 18:20:44 +0400

    build: replace atl with ts-loader move "lib" in tsconfig.json to the approtriate field

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
-const { CheckerPlugin } = require('awesome-typescript-loader');
 console.log(process.env.NODE_ENV);
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -9,17 +8,11 @@ module.exports = {
   externals: [nodeExternals()],
   entry: './src/bin/index.ts',
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'awesome-typescript-loader',
-        exclude: /node_modules/,
-      },
-    ],
+    rules: [{ test: /\.ts?$/, loader: 'ts-loader' }],
   },
   devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : undefined,
   watch: process.env.NODE_ENV === 'development',
-  plugins: [new CheckerPlugin(), new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
+  plugins: [new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
   resolve: {
     extensions: ['.ts', '.js'],
   },
